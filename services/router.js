b0901cda59f9af9b52fa387203c6c238e523de2a
Jared Thomas <jaredthomas@Jareds-MacBook-Air.local>
2017-08-06 23:17:01 -0400

    finished, mlab time

--- services/router.js
+++ services/router.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 
-
 const AuthenticationController = require('../controllers/authentication_controller');
+const TodosController = require('../controllers/todos_controller');
 const passportService = require('./passport');
 
 var requireAuth = passport.authenticate('jwt', {session: false});
@@ -16,6 +16,14 @@ router.route('/protected').get(requireAuth, protected);
 
 router.route('/signup').post(AuthenticationController.signup);
 
-router.route('/signin').post([requireLogin, AuthenticationController.signin])
+router.route('/signin').post([requireLogin, AuthenticationController.signin]);
+
+router.route('/users/:user_id/todos')
+.post(requireAuth, TodosController.create)
+.get(requireAuth, TodosController.index);
+
+
+router.route('/users/:user_id/todos/:todo_id')
+	.delete(requireAuth, TodosController.destroy);
 
 module.exports = router;
